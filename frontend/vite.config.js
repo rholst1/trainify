8299a258368a8b3a54f5ff32b7de4e99ac6d7ef1
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2021-12-09 15:31:40 +0100

    Revert "Added npm run all and inserted greeting code in frontend + backend"
    This reverts commit b06f8a791e65a7ab7df1df7960627ba105670cfd.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // all fetches starting with '/api' will go to backend
-      '/api': 'http://localhost:3000' // same port as backend
-    }
-  }
+  plugins: [react()]
 })
-
