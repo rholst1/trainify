de89274746f2694d29dcf9ae86158ca4827c2f24
Randamandawi <70172778+Randamandawi@users.noreply.github.com>
2021-12-14 13:32:19 +0100

    StripeInit

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,45 @@
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
+const express = require("express")
+const cors = require("cors")
+const stripe = require("stripe")("sk_test_51K6WavLuMgncR3MOQBWdMzbytWTJ7ySxcJel0RjjQyAgLC39rk88VqkphcCrKkEgPKOdxeLjAMARSHZN8WGW1tRP00yoXqSBj0")
+//const uuid = require("uuid/v4")
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+//middelware
+app.use(express.json())
+app.use(cors())
+
+
+const PORT = process.env.PORT || 3001
+
+// View Engine Setup 
+app.set("view engine", "ejs");
+
+//routes
+app.get("/",(req, res) =>{ 
+  res.send("test Randa")
 });
 
+app.post("/payment", (req,res) => {
+
+  const {product, token} = req.body;
+  console.log("Product", product);
+  console.log("Price", product.price);
+  //const idempontencyKey = uuid()
+
+  return stripe.customers.create({
+    email: token.email,
+    source: token.id
+  }).then(customer => {
+    stripe.charges.create({})
+  }).then(result => res.status(200).json(result))
+  .catch(err => console.log(err))
+
+})
+
+
+
+//listen
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
