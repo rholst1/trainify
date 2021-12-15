e65c95c911689daef68c7499522febd01639beab
Randamandawi <70172778+Randamandawi@users.noreply.github.com>
2021-12-15 15:11:54 +0100

    SearchComponentReact done

--- frontend/src/Home/HomePage.js
+++ frontend/src/Home/HomePage.js
@@ -1,41 +1,27 @@
 //import React from "react";
 import StripePayment from "../Stripe/StripePayment";
-import Calendar from 'react-calendar';
 import React, { useEffect, useState } from 'react';
-import Clock from 'react-clock';
-import 'react-calendar/dist/Calendar.css';
+import DateTimePicker from 'react-datetime-picker';
+//import 'react-calendar/dist/Calendar.css';
+
+//import 'react-clock/dist/Clock.css';
 import './HomePage.css'
 
 const HomePage = () => {
 
     const [value, onChange] = useState(new Date());
-    const [value2, setValue] = useState(new Date());
-    console.log(value)
-    useEffect(() => {
-        const interval = setInterval(
-          () => setValue(new Date()),
-          1000
-        );
-    
-        return () => {
-          clearInterval(interval);
-        }
-      }, []);
-
     
     return (
         <>
-            Sök en resa
+            <p>Planera din resa</p>
             Från <input typ="text"></input> 
             Till <input typ="text"></input>
             <button>Hitta resa</button>
-            <div className="calender">
-            <p>Current time:</p>
-            <Clock value={value2}/>
-                <Calendar
-                    onChange={onChange}
-                    value={value}
-                />
+            <div> 
+                 <DateTimePicker
+            onChange={onChange}
+            value={value}
+      />
             </div>
             <StripePayment />
 
@@ -43,5 +29,4 @@ const HomePage = () => {
 
     )
 }
-
 export default HomePage
