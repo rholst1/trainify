6dd8bf6613cdef82a660dcac0c4cfc6fe0977d89
Randamandawi <70172778+Randamandawi@users.noreply.github.com>
2021-12-15 13:14:41 +0100

    SearchComponent

--- frontend/src/Home/HomePage.js
+++ frontend/src/Home/HomePage.js
@@ -1,16 +1,30 @@
-import React from "react";
+//import React from "react";
 import StripePayment from "../Stripe/StripePayment";
+import Calendar from 'react-calendar';
+import React, { useEffect, useState } from 'react';
+import Clock from 'react-clock';
+import 'react-calendar/dist/Calendar.css';
+import './HomePage.css'
 
 const HomePage = () => {
 
+    const [value, onChange] = useState(new Date());
     return (
         <>
-            Hello page
-            <input typ="text"></input>
-            <input typ="text"></input>
-
+            Sök en resa
+            Från<input typ="text"></input>
+            Till<input typ="text"></input>
+            <button>Hitta resa</button>
+        <div class = "calender">
+      <Calendar
+        onChange={onChange}
+        value={value}
+      />
+    </div> 
             <StripePayment/>
+          
         </>
+        
     )
 }
 
