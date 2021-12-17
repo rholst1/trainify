796a89b098dbe352e459b3e8724c5377fa56155e
Randamandawi <70172778+Randamandawi@users.noreply.github.com>
2021-12-17 13:49:20 +0100

    Database upp and running

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
+const path = require('path');
 const express = require("express")
 const cors = require("cors")
 const stripe = require("stripe")("sk_test_51K6WavLuMgncR3MOQBWdMzbytWTJ7ySxcJel0RjjQyAgLC39rk88VqkphcCrKkEgPKOdxeLjAMARSHZN8WGW1tRP00yoXqSBj0")
 //const uuid = require("uuid/v4")
-
+const sqlite3 = require("sqlite3").verbose();
 const app = express();
-
+const db_name = path.join(__dirname, "data", "database.db");
+// connect to the database
+const db = new sqlite3.Database(db_name, err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Successful connection to the database 'apptest.db'");
+});
 //middelware
 app.use(express.json())
 app.use(cors())
