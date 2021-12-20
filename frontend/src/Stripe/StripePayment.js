2584e63a40594ce8812a4448bc593fbdb1e637e9
SaAlgervik <70169634+SaAlgervik@users.noreply.github.com>
2021-12-20 15:18:11 +0100

    Update StripePayment.js

--- frontend/src/Stripe/StripePayment.js
+++ frontend/src/Stripe/StripePayment.js
@@ -34,7 +34,7 @@ const StripePayment = () => {
     return (
         <>
             <StripeCheckout
-                stripeKey='pk_test_51K6WavLuMgncR3MOChxwZs8AmBuPekI45L0kvP16HW6TDuFKEojrJ1OjkEUWWJZLTnMCpBiUK9zY8UyshaJP2aTC00rVyiRaqm'
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
                 token={makePayment}
                 name='By Ticket '
             >
