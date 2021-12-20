dbe0c9cc162554bad2d0f5a5550922f08961c1c6
sholmqvist <sholmqvist89@gmail.com>
2021-12-20 10:43:10 +0100

    Added GET API method for DB
    Added a simple API GET method and filled the DB with some dummy data. Tested OK

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,22 @@ const express = require("express")
 const cors = require("cors")
 const stripe = require("stripe")("sk_test_51K6WavLuMgncR3MOQBWdMzbytWTJ7ySxcJel0RjjQyAgLC39rk88VqkphcCrKkEgPKOdxeLjAMARSHZN8WGW1tRP00yoXqSBj0")
 //const uuid = require("uuid/v4")
-const sqlite3 = require("sqlite3").verbose();
+
+// const sqlite3 = require("sqlite3").verbose();
+
 const app = express();
-const db_name = path.join(__dirname, "data", "database.db");
+
+// const db_name = path.join(__dirname, "data", "database.db");
 // connect to the database
-const db = new sqlite3.Database(db_name, err => {
+/* const db = new sqlite3.Database(db_name, err => {
   if (err) {
     return console.error(err.message);
   }
   console.log("Successful connection to the database 'apptest.db'");
 });
+
+*/
+
 //middelware
 app.use(express.json())
 app.use(cors())
@@ -52,3 +58,27 @@ app.post("/payment", (req,res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+  // Driver for better-sqlite3
+const dbDriver = require('better-sqlite3');
+
+  // Database connector with DB path
+const dbPath = dbDriver('./backend/data/database.db');
+
+
+  // GET Function that grabs tablename and ID dynamically
+  // example: localhost:3001/api/Ticket/1 <--- Get you all the data from Ticket table with ID 1
+app.get('/api/:table/:id', (request, response) => {
+
+  let fetchIdFromDatabase = dbPath.prepare(`
+      SELECT * 
+      FROM ${request.params.table}
+      WHERE id = :id
+      `);
+
+  let result = fetchIdFromDatabase.all({id: request.params.id});
+
+  response.json(result);
+  console.log(result);
+
+  });
