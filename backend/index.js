276ab66c78e9146fdfa4185d4d75d99ac9307dac
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2021-12-21 09:31:18 +0100

    Installed nodemailer and created configuration

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+
+
 const path = require('path');
 const express = require("express")
 const cors = require("cors")
@@ -128,3 +130,39 @@ app.post('/api/:table', (request, response) => {
   
 });
 */
+
+const nodemailer = require("nodemailer");
+const output =
+  '<p> You have a new contact request </p>'
+'<h3> Contact Details</h3>';
+
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <test@example.com>', // sender address
+    to: "julia.solbacken@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: output, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+}
+main().catch(console.error);
