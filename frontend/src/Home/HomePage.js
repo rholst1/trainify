c8ecaec44a983097beacbb501660e46f07d97fe2
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2022-01-10 11:28:40 +0100

    Adds mindate for calendarar preventing attempts to book past dates

--- frontend/src/Home/HomePage.js
+++ frontend/src/Home/HomePage.js
@@ -1,86 +1,77 @@
-import StripePayment from "../Components/Stripe/StripePayment";
+import StripePayment from '../Components/Stripe/StripePayment';
 import React, { useState } from 'react';
 import DateTimePicker from 'react-datetime-picker';
 import './HomePage.css';
-import SearchStation from "../Components/Search/SearchStation";
+import SearchStation from '../Components/Search/SearchStation';
 import { FaRegArrowAltCircleRight } from 'react-icons/fa';
-import SearchButton from "../Components/Button/SearchButton"
+import SearchButton from '../Components/Button/SearchButton';
 
-import Image from "../Components/Image/Image";
-import Booking from "../Components/Booking/Booking";
-import { handle } from "express/lib/application";
+import Image from '../Components/Image/Image';
+import Booking from '../Components/Booking/Booking';
+import { handle } from 'express/lib/application';
 
 const HomePage = () => {
-    var date = new Date();
-    const [value, setValue] = useState(date);
-    const [stationOne, setStationOne] = useState("Från:");
-    const [stationTwo, setStationTwo] = useState("Till:");
-    const [search, setSearch] = useState(false)
-    const [station, setStation] = useState('')
-    // console.log(process.env.REACT_APP_STRIPE_KEY)
-    console.log(search)
+  var date = new Date();
+  const [value, setValue] = useState(date);
+  const [stationOne, setStationOne] = useState('Från:');
+  const [stationTwo, setStationTwo] = useState('Till:');
+  const [search, setSearch] = useState(false);
+  const [station, setStation] = useState('');
+  // console.log(process.env.REACT_APP_STRIPE_KEY)
 
+  let today = new Date();
 
+  console.log(search);
 
-    return (
-        <>
-            <div className="Wrapper">
-                <div className='Section'>
-                </div>
-                <Image />
-                {!search ?
-                    <div>
-                        <div className="PageHeader" >
-                            <h1>Planera din resa </h1>
-                        </div>
-
-                        <div className="InputContainer">
-                            <SearchStation
-                                input={stationOne}
-                                setValue={setStationOne}
-                            />
-                            <div className="IconContainer">
-                                <FaRegArrowAltCircleRight
-                                    className="Icon"
-                                    fontSize='43px'
-                                />
-                            </div>
-                            <SearchStation
-                                input={stationTwo}
-                                setValue={setStationTwo}
-                            />
-                        </div>
-
-                        <div className="DateTimeContainer">
-                            <DateTimePicker
-                                className='DateTime'
-                                onChange={setValue}
-                                value={value}
-                                locale="se"
-                                disableClock="true"
-                            />
-                        </div>
-                        <SearchButton
-                            text='Hitta Resa'
-                            setSearch={setSearch}
-                        />
-                    </div>
-                    :
-                    <p></p>
-                }
-                {search ?
-                    <Booking
-                        fromStation={stationOne}
-                        toStation={stationTwo}
-                        d={value}
-                        setSearch={setSearch}
-                        setStation={setStation}
-                    />
-                    :
-                    <div><p>{station} </p></div>
-                }
+  return (
+    <>
+      <div className="Wrapper">
+        <div className="Section"></div>
+        <Image />
+        {!search ? (
+          <div>
+            <div className="PageHeader">
+              <h1>Planera din resa </h1>
             </div>
-        </>
-    )
-}
-export default HomePage
+
+            <div className="InputContainer">
+              <SearchStation input={stationOne} setValue={setStationOne} />
+              <div className="IconContainer">
+                <FaRegArrowAltCircleRight className="Icon" fontSize="43px" />
+              </div>
+              <SearchStation input={stationTwo} setValue={setStationTwo} />
+            </div>
+
+            <div className="DateTimeContainer">
+              <DateTimePicker
+                className="DateTime"
+                onChange={setValue}
+                value={value}
+                locale="se"
+                disableClock="true"
+                minDate={today}
+              />
+            </div>
+            <SearchButton text="Hitta Resa" setSearch={setSearch} />
+          </div>
+        ) : (
+          <p></p>
+        )}
+        {search ? (
+          <Booking
+            fromStation={stationOne}
+            toStation={stationTwo}
+            d={value}
+            setSearch={setSearch}
+            setStation={setStation}
+          />
+        ) : (
+          <div>
+            <p>{station} </p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+export default HomePage;
