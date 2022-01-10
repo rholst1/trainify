6d88035c0f2706000a64a4902b62474b69332ce8
SaAlgervik <70169634+SaAlgervik@users.noreply.github.com>
2022-01-10 15:26:09 +0100

    Update HomePage.js

--- frontend/src/Home/HomePage.js
+++ frontend/src/Home/HomePage.js
@@ -11,67 +11,67 @@ import Booking from '../Components/Booking/Booking';
 import { handle } from 'express/lib/application';
 
 const HomePage = () => {
-  var date = new Date();
-  const [value, setValue] = useState(date);
-  const [stationOne, setStationOne] = useState('Från:');
-  const [stationTwo, setStationTwo] = useState('Till:');
-  const [search, setSearch] = useState(false);
-  const [station, setStation] = useState('');
-  // console.log(process.env.REACT_APP_STRIPE_KEY)
+    var date = new Date();
+    const [value, setValue] = useState(date);
+    const [stationOne, setStationOne] = useState('Från:');
+    const [stationTwo, setStationTwo] = useState('Till:');
+    const [search, setSearch] = useState(false);
+    const [station, setStation] = useState('');
+    // console.log(process.env.REACT_APP_STRIPE_KEY)
 
-  let today = new Date();
+    let today = new Date();
 
-  console.log(search);
+    console.log(search);
 
-  return (
-    <>
-      <div className="Wrapper">
-        <div className="Section"></div>
-        <Image />
-        {!search ? (
-          <div>
-            <div className="PageHeader">
-              <h1>Planera din resa </h1>
+    return (
+        <>
+            <div className="Wrapper">
+                <div className="Section"></div>
+                <Image />
+                {!search ? (
+                    <div>
+                        <div className="PageHeader">
+                            <h1>Planera din resa </h1>
+                        </div>
+
+                        <div className="InputContainer">
+                            <SearchStation input={stationOne} setValue={setStationOne} />
+                            <div className="IconContainer">
+                                <FaRegArrowAltCircleRight className="Icon" fontSize="43px" />
+                            </div>
+                            <SearchStation input={stationTwo} setValue={setStationTwo} />
+                        </div>
+
+                        <div className="DateTimeContainer">
+                            <DateTimePicker
+                                className="DateTime"
+                                onChange={setValue}
+                                value={value}
+                                locale="se"
+                                disableClock="true"
+                                minDate={today}
+                            />
+                        </div>
+                        <SearchButton text="Hitta Resa" setSearch={setSearch} />
+                    </div>
+                ) : (
+                    <p></p>
+                )}
+                {search ? (
+                    <Booking
+                        fromStation={stationOne}
+                        toStation={stationTwo}
+                        d={value}
+                        setSearch={setSearch}
+                        setStation={setStation}
+                    />
+                ) : (
+                    <div>
+                        <p>{station} </p>
+                    </div>
+                )}
             </div>
-
-            <div className="InputContainer">
-              <SearchStation input={stationOne} setValue={setStationOne} />
-              <div className="IconContainer">
-                <FaRegArrowAltCircleRight className="Icon" fontSize="43px" />
-              </div>
-              <SearchStation input={stationTwo} setValue={setStationTwo} />
-            </div>
-
-            <div className="DateTimeContainer">
-              <DateTimePicker
-                className="DateTime"
-                onChange={setValue}
-                value={value}
-                locale="se"
-                disableClock="true"
-                minDate={today}
-              />
-            </div>
-            <SearchButton text="Hitta Resa" setSearch={setSearch} />
-          </div>
-        ) : (
-          <p></p>
-        )}
-        {search ? (
-          <Booking
-            fromStation={stationOne}
-            toStation={stationTwo}
-            d={value}
-            setSearch={setSearch}
-            setStation={setStation}
-          />
-        ) : (
-          <div>
-            <p>{station} </p>
-          </div>
-        )}
-      </div>
-    </>
-  );
+        </>
+    );
 };
 export default HomePage;
